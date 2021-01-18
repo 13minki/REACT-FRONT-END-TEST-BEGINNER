0081ada385039a191addf685d82339799208cf77
13minki <lalita.l@kkumail.com>
2021-01-18 16:44:31 +0700

    Add new Todolist
    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Button, Card, Input, Space } from "antd";
 import styled from "styled-components";
+import React , { useState , useEffect } from 'react';
 
 const Container = styled.div`
   display: flex;
@@ -11,22 +12,79 @@ const Container = styled.div`
   padding: 16px 24px;
 `;
 
+
 function App() {
+
+  const [todoList,setTodoList] = useState([
+      {
+         title: "Defualt1",
+         isDone: false
+      },
+      {
+         title: "Defualt2",
+         isDone: true
+      }
+    ]);
+  const [item,setItem]= useState('');
+
+  const addNewItem = () => {
+      //console.log(item);
+      const newItem = [
+        {
+           title: item,
+           isDone: false
+         }
+      ];
+      //console.log(newItem);
+      const newItemList = [
+        ...todoList,
+        ...newItem
+      ];
+      setTodoList(newItemList);
+      console.log(newItemList);
+      console.log(todoList);
+  }
+
+  const CheckIsDone = (props) => {
+    console.log(props);
+    const isDone = props.isDone;
+    if(isDone) {
+      <h1>me true</h1>
+    } else {
+        <h1>me false</h1>
+    }
+  }
+
+  useEffect (() => {
+    console.log("effect");
+    }, [])
+
+  const MyMinki = () => {
+    console.log("my Minki");
+  }
+
   return (
     <Container>
       <Space>
-        <Input style={{ width: 400 }} placeholder="Enter Task Name" />
-        <Button type="primary">Create Task</Button>
+        <Input type="text" style={{ width: 400 }} placeholder="Enter Task Name" onChange={event => setItem(event.target.value)} />
+        <Button type="primary" onClick={ () => addNewItem() }>Create Task</Button>
       </Space>
-      <Space direction="vertical" style={{ marginTop: 24 }}>
-        <Card title="Sample Task" style={{ width: 600 }}>
-          <Button style={{ marginRight: "16px" }} type="primary">
-            Duplicate
-          </Button>
-          <Button style={{ marginRight: "16px" }}>Done</Button>
-          <Button danger>Delete</Button>
-        </Card>
-      </Space>
+      
+      <div className="tasks">
+        {todoList.map((item, index) => (
+            <div>
+              <Space direction="vertical" style={{ marginTop: 24 }}>
+                <Card title={item.title} style={{ width: 600 }}>
+                  <Button style={{ marginRight: "16px" }} type="primary">Duplicate</Button>
+                  <Button style={{ marginRight: "16px" }} >Done</Button>
+                  <Button danger>Delete</Button>
+                </Card>
+              </Space>
+
+            </div>
+        ))}
+      </div>
+
       <Space direction="vertical" style={{ marginTop: 24 }}>
         <Card
           title="Sample Task (Done)"
@@ -39,8 +97,12 @@ function App() {
           <Button danger>Delete</Button>
         </Card>
       </Space>
+
+
     </Container>
   );
+
+
 }
 
 export default App;
